8ae2a6c7e680f292e91cb152b1676df20c8b6574
Akash Gangil <akash.gangil@outlook.com>
2013-12-04 19:44:59 -0500

    cleaning up

--- src/main.c
+++ src/main.c
@@ -27,77 +27,52 @@ void main(void)
 
     char buffer[150];
     while(fgets(buffer,10,input_file)){
-//        printf("Buffer is %s\n", buffer);
         strcpy(input_data+i,buffer);
         i = i+9;
     }
 
     printf("The Input Data is %s\n", input_data);
-    printf("Input Data Length %d\n", strlen(input_data));
-    //fscanf(input_file, "%s", input_data);
     fclose(input_file);
-
-    //strcat(input_data, "\0");
-
-    //printf("Test is %s\n", input_data);
-
-    //Print the check value for the selected CRC algorithm.
-    //printf("Standard is %s\n", CRC_NAME);
 
     struct stopwatch_t* sw = stopwatch_create();
 
     stopwatch_init();
     stopwatch_start(sw);
 
-    // Compute the CRC of the test message, slowly.
-    printf("The crcSlow() of %s is 0x%X\n", input_data, crcSlow(input_data, strlen(input_data)));
+    printf("crcSlow() 0x%X  ", crcSlow(input_data, strlen(input_data)));
 
     stopwatch_stop(sw);   
 
-    //printf("%Lg\n", stopwatch_elapsed(sw));
+    printf("  Time: %Lg\n", stopwatch_elapsed(sw));
 
     stopwatch_start(sw);
 
     size_t input_data_len = strlen(input_data);
     size_t input_block_size = input_data_len / INPUT_BLOCKS;
 
-    printf("Input Block Size: %d\n", input_block_size);
-
     char* input_data1 = malloc(input_block_size+1);
     strncpy(input_data1, input_data, input_block_size);
     *(input_data1 + input_block_size) = '\0';
 
-    printf("Part 1 data is %s\n", input_data1);
-    printf("Part 1 Data Length:  %d\n", strlen(input_data1));
-
     int result1 = crcSlow(input_data1, input_block_size);
-    //printf("Part 1: %X\n", result1);
-
 
     char* input_data2 = malloc(input_block_size+1);
     strncpy(input_data2, input_data + input_block_size, input_block_size);
     strcat(input_data2, "\0");
 
-    printf("Part 2 data is %s\n", input_data2);
-    printf("Part 2 Data Length: %d\n", strlen(input_data2));
-
     int result2 = crcSlow(input_data2, input_block_size);
-    //printf("Part 2: %X\n", result2);
 
-
-    //printf("INPUT_BLOCK_SIZE: %d\n", input_block_size);
     unsigned long ans = crc32_combine(result1, result2, input_block_size);
 
     stopwatch_stop(sw);
 
-    printf("Combined CRC 0x%lx     %Lg \n",ans, stopwatch_elapsed(sw));
+    printf("Combined CRC 0x%lx  Time:  %Lg \n",ans, stopwatch_elapsed(sw));
 
-    //Compute the CRC of the test message, more efficiently.	 
     crcInit();
     stopwatch_start(sw);
-    printf("The crcFast()  (Table Lookup) of %s is 0x%X\n", input_data, crcFast(input_data, strlen(input_data)));
+    printf("crcFast() 0x%X  ", crcFast(input_data, strlen(input_data)));
     stopwatch_stop(sw);
-    //printf("%Lg\n", stopwatch_elapsed(sw));
+    printf("  Time: %Lg\n", stopwatch_elapsed(sw));
 
     stopwatch_destroy(sw);
 } 
