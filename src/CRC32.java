3f1c0f8f05eeeb6027d5e3ec36b50eeb77deccbe
Akash Gangil <akash.gangil@outlook.com>
2013-12-07 14:23:39 -0500

    update the java code

--- src/CRC32.java
+++ src/CRC32.java
@@ -1,14 +1,12 @@
-/********************************
- * CRC-32 MPEG Java Implementation
- *********************************/
+/*JAVA CRC32 implementation using MPEG Polynomial*/
 
+import java.util.*;
+import java.util.Arrays.*;
 
 public class CRC32 { 
 
     public static void main(String[] args) { 
-
         byte[] bytes = args[0].getBytes();
-
         java.util.zip.CRC32 x = new java.util.zip.CRC32();
         x.update(bytes);
         System.out.println("CRC32 (via Java's library)     = " + Long.toHexString(x.getValue()));
@@ -16,4 +14,5 @@ public class CRC32 {
     }
 
 }
+
 
