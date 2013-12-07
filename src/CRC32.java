360e8c434969d1c8d74f0e69591e4036690ff56f
Akash Gangil <akash.gangil@outlook.com>
2013-12-07 16:36:02 -0500

    minor fix in the java code

--- src/CRC32.java
+++ src/CRC32.java
@@ -1,16 +1,33 @@
 /*JAVA CRC32 implementation using MPEG Polynomial*/
 
-import java.util.*;
-import java.util.Arrays.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class CRC32 { 
 
-    public static void main(String[] args) { 
-        byte[] bytes = args[0].getBytes();
-        java.util.zip.CRC32 x = new java.util.zip.CRC32();
-        x.update(bytes);
-        System.out.println("CRC32 (via Java's library)     = " + Long.toHexString(x.getValue()));
 
+    private static String readFile(String pathname) throws IOException {
+
+        InputStream fstream = new FileInputStream("input.in");
+        DataInputStream in = new DataInputStream(fstream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String strLine = br.readLine();
+        return strLine.substring(0, strLine.length()-1);
+    }
+
+
+    public static void main(String[] args) { 
+        try{
+            String fileData = readFile("input.in");
+            System.out.print(fileData);
+            System.out.print("\n");
+            byte[] bytes = fileData.getBytes();
+            java.util.zip.CRC32 x = new java.util.zip.CRC32();
+            x.update(bytes);
+            System.out.println("CRC32 (via Java's library)     = " + Long.toHexString(x.getValue()));
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
 }
