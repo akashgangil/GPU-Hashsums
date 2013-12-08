2bf4fdd35857c1f136e1c747d6cd4d0f8f038694
Akash Gangil <akash.gangil@outlook.com>
2013-12-08 16:13:33 -0500

    write in blocks instead of one big chunk to avoid memory error

--- src/randomMessage.py
+++ src/randomMessage.py
@@ -3,10 +3,11 @@ import random
 import sys
 
 def randomMessage(n):
-    lst = [random.choice(string.ascii_letters + string.digits) for n in xrange(int(n))]
-    message = "".join(lst)
-    f = open('input.in', 'w')
-    f.write(message)
+    f = open('input.in', 'w+')
+    for i in xrange(int(n)):
+      lst = [random.choice(string.ascii_letters + string.digits) for n in xrange(1000000)]
+      message = "".join(lst)
+      f.write(message)
     f.close()
     
 if __name__ == "__main__":
