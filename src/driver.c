7ab4ac3021ab09557a074cc829ac86c6dda20699
Lup Peng Loke <lloke3@jinx-login.cc.gatech.edu>
2013-12-08 12:17:33 -0500

    updating the driver file as wel

--- src/driver.c
+++ src/driver.c
@@ -10,40 +10,42 @@
 
 int main (int argc, char** argv){
 
-    /* declare variables */
-    char *h_A, *d_A, ans;
+	// Declare variables
+		char* d_A;
+		char* combinedCRC = malloc(4); 	//allocate 4 bytes (32 bits) for CRC Checksum
+		char* input_data = malloc(DATA_SIZE);
+		int i = 0;
 
-    char* input_data = malloc(DATA_SIZE);
+	// Loading and processing the input data from file 
+		FILE* input_file;
+		input_file = fopen(INPUT_FILE, "r");
 
-    FILE* input_file;
-    input_file = fopen(INPUT_FILE, "r");
+		if(input_file == NULL){
+			fprintf(stderr, "Failed to open %s\n", INPUT_FILE);
+			exit(1);
+		}
+		
+		char buffer[BLOCK_SIZE];
+		while(fgets(buffer, BLOCK_SIZE, input_file)){
+			strcpy(input_data+i,buffer);
+			i = i + BLOCK_SIZE - 1;
+		}
+		fclose(input_file);
 
-    if(input_file == NULL){
-        fprintf(stderr, "Failed to open %s\n", INPUT_FILE);
-        exit(1);
-    }
+		/*Replace the line field ascii with \0*/
+		input_data[strlen(input_data) - 1] = '\0';
 
-    int i = 0;
+		
+	//Calling function to copy the input data ot the device memory
+    		initCudaArray (&d_A, input_data, strlen(input_data) + 1);
+	
+    
+	//Calculate CRC - call the kernel in the function below:
+	    	cudaCRC (d_A, strlen(input_data) + 1, combinedCRC);
+		fprintf (stderr, "Program Ended Successfully!\n");
 
-    char buffer[BLOCK_SIZE];
-    while(fgets(buffer, BLOCK_SIZE, input_file)){
-        strcpy(input_data+i,buffer);
-        i = i + BLOCK_SIZE - 1;
-    }
-    fclose(input_file);
+	//Free up variables 
+	    	free (input_data);
 
-    /*Replace the line field ascii with \0*/
-    input_data[strlen(input_data) - 1] = '\0';
-    printf("The Input Data is %s\n", input_data);
-
-    /*Modify it to input string */
-    initCudaArray (&d_A, input_data, strlen(input_data) + 1);
-
-    /* do reduction */
-    cudaCRC (d_A, strlen(input_data) + 1, &ans);
-
-    free (input_data);
-
-    return 0;
+	return 0;
 }
-
