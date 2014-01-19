0cc8edf3adc5dccbdb67e177d14dcca07055f91a
Akash Gangil <akash.gangil@outlook.com>
2014-01-18 21:31:37 -0500

    cleanup

--- crc_intel.h
+++
@@ -1,8 +0,0 @@
-#include<stdint.h>
-
-#ifndef CRC_INTEL_H
-#define CRC_INTEL_H
-
-uint32_t CrcHash(const void* data, uint32_t bytes);
-
-#endif
--- parallel_crc.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARALLEL_CRC_H
-#define PARALLEL_CRC_H
-
-
-unsigned long crc32_combine(unsigned long crc1, unsigned long crc2, unsigned long len2);
-
-#endif
